71cc084b49ef8ffd66cdb959b857b3272700ad3e
andreyZozulych <it.andbussines@gmail.com>
2023-03-06 07:41:43 -0600

    Initial Commit

---
+++ lesson2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson2
+//
+//  Created by Andrii Zozulych on 06.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
